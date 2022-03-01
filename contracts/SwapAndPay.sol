209c4333d34161fbbebe27783d23657e0fc7b786
0xMarty <tmpiercet@gmail.com>
2022-03-01 14:42:51 -0500

    Smol change

--- contracts/SwapAndPay.sol
+++ contracts/SwapAndPay.sol
@@ -1,5 +1,15 @@
 pragma solidity 0.8.10;
 
+interface IERC20 {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
 interface IRouter {
     function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
         external
@@ -8,7 +18,7 @@ interface IRouter {
 /// @title Swap And Pay contract
 /// @author 0xMarty
 /// @notice Swaps from ERC20 to ETH and pays specified address
-contract PayAndSwap {
+contract SwapAndPay {
 
     ///STATE VARIABLES///
 
@@ -51,9 +61,13 @@ contract PayAndSwap {
     ) external {
         require(msg.sender == owner, "not owner");
 
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+
         address[] memory path = new address[](2);
         path[0] = _token;
         path[1] = WETH;
+
+        IERC20(_token).approve(_router, _amount);
 
         IRouter(_router).swapExactTokensForETH(_amount, _minETH, path, address(this), block.timestamp + 10);
         
